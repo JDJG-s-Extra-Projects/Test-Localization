b34e2e4c773e52b678a4938f1e41a65bc9ea6d58
JDJG <22802640+JDJGInc@users.noreply.github.com>
2024-06-17 10:56:42 -0400

    Alright should be ready to try.

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 import logging
 import os
+import traceback
 from typing import Any, Optional
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from cogs import EXTENSIONS
 from command_tree import JDCommandTranslator, JDCommandTree
 
 
@@ -19,6 +21,12 @@ class JDBot(commands.Bot):
 
     async def setup_hook(self) -> None:
         await self.tree.set_translator(JDCommandTranslator())
+
+    for cog in EXTENSIONS:
+        try:
+            await self.load_extension(f"{cog}")
+        except commands.errors.ExtensionError:
+            traceback.print_exc()
 
 
 intents = discord.Intents.all()
